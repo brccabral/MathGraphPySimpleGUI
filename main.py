3412c057579cabfd66a22478527cbbfa810c4e22
Bruno Cabral <cabral.brc@gmail.com>
2022-04-05 18:25:45 -0700

    added graph to window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 sg.theme("DarkTeal6")
 table_content = []
@@ -13,9 +15,18 @@ layout = [
         )
     ],
     [sg.Input(key="-INPUT-", expand_x=True), sg.Button("Submit", key="-SUBMIT-")],
+    [sg.Canvas(key="-CANVAS-")],
 ]
 
-window = sg.Window("Math Graph", layout)
+# finalize is need because we are adding the graph to the window before calling window.read()
+window = sg.Window("Math Graph", layout, finalize=True)
+
+# matplotlib setup
+fig = Figure(figsize=(5, 4))
+fig.add_subplot(111).plot([], [])
+figure_canvas_agg = FigureCanvasTkAgg(fig, window["-CANVAS-"].TKCanvas)
+figure_canvas_agg.draw()
+figure_canvas_agg.get_tk_widget().pack()
 
 while True:
     event, values = window.read()
